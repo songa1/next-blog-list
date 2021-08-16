7e33e4ead8482de2fe26df161eab28f1c8138ed4
songa1 <songachillethe1her@gmail.com>
2021-08-16 10:25:59 +0200

    removed redirection

--- pages/404.js
+++ pages/404.js
@@ -1,20 +1,20 @@
 import Link from 'next/link'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
+// import { useEffect } from 'react'
+// import { useRouter } from 'next/router'
 
 const NotFound = () => {
-    const router = useRouter();
+    // const router = useRouter();
 
-    useEffect(() =>{
-        setTimeout(() => {
-            router.push('/')
-        }, 5000)
-    },[])
+    // useEffect(() =>{
+    //     setTimeout(() => {
+    //         router.push('/')
+    //     }, 5000)
+    // },[])
 
     return (
         <div className='not-found'>
             <h1>Ooooops...</h1>
-            <h2>The page you're looking for, is not found.</h2>
+            <h2>The page you&apos;re looking for, is not found.</h2>
             <p>Go back to the <Link href="/"><a>Homepage</a></Link></p>
         </div>
     )
